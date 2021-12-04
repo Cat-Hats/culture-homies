437530d387c95fe3b5d51f03ac052af8e38b01bc
joelmatiasduran <joelmatiasduran@gmail.com>
2021-12-03 20:01:11 -0500

    Test

--- components/Hero/Index.tsx
+++ components/Hero/Index.tsx
@@ -3,6 +3,10 @@ import React from "react";
 interface IndexProps {}
 
 const Index: React.FC<IndexProps> = ({}) => {
-  return <>Hi I am a future responsive Hero</>;
+  return (
+    <>
+      <div>Hi I am a future responsive Hero</div>
+    </>
+  );
 };
 export default Index;
