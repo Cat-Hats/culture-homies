8d584fecd7669ad26cfe83d064efcc26bcf5d7fe
joelmatiasduran <joelmatiasduran@gmail.com>
2021-12-03 19:29:20 -0500

    Typescript

---
+++ components/Navbar/Index.tsx
@@ -0,0 +1,12 @@
+import { NavContainer } from "./NavbarElements";
+
+interface Navbar {}
+
+const Navbar: React.FC = () => {
+  return (
+    <>
+      <NavContainer></NavContainer>
+    </>
+  );
+};
+export default Navbar;
